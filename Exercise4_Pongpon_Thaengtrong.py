cf66f2237fa14198fdd77670746b9fb16e0b4d37
giantaok <77913709+giantaok@users.noreply.github.com>
2021-01-24 14:26:34 +0700

    Add files via upload

---
+++ Exercise4_Pongpon_Thaengtrong.py
@@ -0,0 +1,21 @@
+'''
+Foundation English
+'''
+FE  = 85.5
+'''
+General Business
+'''
+GB  = 70
+'''
+Introduction to Computer Systems
+'''
+ICS = 55.2
+'''
+Computer Programming
+'''
+CP  = 10.0
+
+print("Foundation English :", FE)
+print("General Business :", GB)
+print("Introduction to Computer Systems :", ICS)
+print("Computer Programming :", CP)
