48ce1ddd5f15f765d620e428e1921b0902627de2
giantaok <77913709+giantaok@users.noreply.github.com>
2021-01-25 12:48:30 +0700

    Add files via upload

---
+++ Exercise5_1_pongpon_T.py
@@ -0,0 +1,7 @@
+n1 = int(input("ตัวเลขที่1 :"))
+n2 = int(input("ตัวเลขที่2 :"))
+
+print(n1, ("+"), n2, n1+n2)
+print(n1, ("-"), n2, n1-n2)
+print(n1, ("*"), n2, n1*n2)
+print(n1, ("/"), n2, n1/n2)
